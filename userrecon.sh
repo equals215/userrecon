1d5c0924f65569a633cd36f287b013e4973a049f
Thomas FOUBERT <thomas@thomas-foubert.com>
2019-06-12 14:01:59 +0200

    Update userrecon.sh : Updated Spotify check

--- userrecon.sh
+++ userrecon.sh
@@ -334,7 +334,7 @@ fi
 ## Spotify
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Spotify: \e[0m"
-check1=$(curl -s -i "https://open.spotify.com/user/$username" -H "Accept-Language: en" -L | grep -o 'HTTP/2 404' ; echo $?)
+check1=$(curl -s -i "https://open.spotify.com/user/$username" -H "Accept-Language: en" -L | grep -o 'HTTP/1.1 404 Not Found' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then
 printf "\e[1;93mNot Found!\e[0m\n"
