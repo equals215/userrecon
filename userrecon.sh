1bc90d7e56a255dffa93dbc72591a7171511906d
Thomas FOUBERT <thomas@thomas-foubert.com>
2019-06-12 12:46:03 +0200

    Update userrecon.sh : Updated Tumblr check with an API call

--- userrecon.sh
+++ userrecon.sh
@@ -163,7 +163,7 @@ fi
 ## TUMBLR
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Tumblr: \e[0m"
-check1=$(curl -s -i "https://$username.tumblr.com" -H "Accept-Language: en" -L --user-agent '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.3) Gecko/20010801"' | grep -o 'HTTP/2 404' ; echo $?)
+check1=$(curl -s -i "api.tumblr.com/v2/blog/$username/avatar" | grep -o '404 Not Found' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then
 printf "\e[1;93mNot Found!\e[0m\n"
