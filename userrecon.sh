7af0a71c91356abf9d1bf07c01e50c15a70664c5
Thomas FOUBERT <thomas@thomas-foubert.com>
2019-06-12 14:29:08 +0200

    Update userrecon.sh : Updated Keybase grep check string

--- userrecon.sh
+++ userrecon.sh
@@ -490,7 +490,7 @@ fi
 ## KeyBase
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Keybase: \e[0m"
-check1=$(curl -s -i "https://keybase.io/$username" -H "Accept-Language: en" -L | grep -o '404 Not Found' ; echo $?)
+check1=$(curl -s -i "https://keybase.io/$username" -H "Accept-Language: en" -L | grep -o 'HTTP/2 404' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then
 printf "\e[1;93mNot Found!\e[0m\n"
