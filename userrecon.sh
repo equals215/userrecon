3ce38844b92f276fc3a7b0516fef367109bf91d7
Thomas FOUBERT <thomas@thomas-foubert.com>
2019-06-12 12:05:20 +0200

    Update userrecon.txt : Removed Google Plus test since Google Plus is only avaliable for G Suite users

--- userrecon.sh
+++ userrecon.sh
@@ -150,7 +150,7 @@ fi
 ## PINTEREST
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Pinterest: \e[0m"
-check1=$(curl -s -i "https://www.pinterest.com/$username" -H "Accept-Language: en" -L --user-agent '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.3) Gecko/20010801"' | grep -o '?show_error' ; echo $?)
+check1=$(curl -s -i "https://www.pinterest.com/$username" -H "Accept-Language: en" -L --user-agent '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.3) Gecko/20010801"' | grep -o 'HTTP/2 404' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then
 printf "\e[1;93mNot Found!\e[0m\n"
